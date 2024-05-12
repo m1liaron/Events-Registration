d4f179456da75ec163ba58f697c81027df390a94
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-05-12 21:16:12 +0300

    get events from api
    Took 24 minutes

--- server/eventsBritte.js
+++ server/eventsBritte.js
@@ -1,26 +1,42 @@
 const axios = require('axios');
 
-// Your Eventbrite API key
-const API_KEY = process.env.EVENT_BRITTE_TOKEN;
+require('dotenv').config();
+const Event = require('./models/Event');
+const connectDB = require('./db/connect');
 
-// URL for Eventbrite's events search endpoint
-const EVENTBRITE_URL = 'https://www.eventbriteapi.com/v3/events/search/';
-
-// Parameters for the events search (you can customize these as needed)
-const params = {
-    token: API_KEY,
-    q: 'music', // Example: searching for music events
-    location: 'San Francisco', // Example: searching in San Francisco
+const options = {
+    method: 'GET',
+    url: 'https://real-time-events-search.p.rapidapi.com/search-events',
+    params: {
+        query: 'Concerts in San-Francisco',
+        start: '0'
+    },
+    headers: {
+        'X-RapidAPI-Key': '812c4f55c7msh8857b9c20af2a37p1a8a08jsn94846147918b',
+        'X-RapidAPI-Host': 'real-time-events-search.p.rapidapi.com'
+    }
 };
 
-// Make a GET request to the Eventbrite API
-axios.get(EVENTBRITE_URL, { params })
-    .then(response => {
-        // Handle the response data (list of events)
-        const events = response.data.events;
-        console.log('List of events:', events);
-    })
-    .catch(error => {
-        // Handle any errors
-        console.error('Error fetching events:', error);
-    });
+async function start(){
+    try {
+        const response = await axios.request(options);
+        let data;
+        for(let key of response.data.data) {
+            data = {
+                title: key.name,
+                description: key.description,
+                event_data:key.start_time,
+                organizer: 'Sternenko'
+            }
+            console.log(data)
+        }
+
+        await connectDB(process.env.MONGO_URI);
+        await Event.create(data);
+        console.log('Success !!!');
+        process.exit(0);
+    } catch (error) {
+        console.error(error);
+    }
+}
+start()
