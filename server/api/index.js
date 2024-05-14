54192aaa062dac0e607041c4526dc367495fc891
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-05-14 18:48:12 +0300

    vercel
    Took 17 minutes

---
+++ server/api/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(express.json());
+app.use(cors());
+
+const connectDB = require('../db/connect')
+
+// Routes
+const eventRouter = require('../routes/eventRoutes')
+const participantRouter = require('../routes/participantRouter')
+app.use('/events', eventRouter)
+app.use('./participants', participantRouter)
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`);
+        });
+    } catch (error) {
+        console.error('Error starting the server:', error);
+    }
+};
+
+start();
